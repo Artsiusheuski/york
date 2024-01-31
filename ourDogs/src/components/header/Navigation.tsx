519741c335791f6161e8e7af57680c5e8bd994f4
Artsiusheuski <i.artsiusheuski@gmail.com>
2024-01-31 21:07:40 +0100

    3t

--- ourDogs/src/components/header/Navigation.tsx
+++ ourDogs/src/components/header/Navigation.tsx
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <ul className="navigator_container">
-      <li>
-        <Link to={"/"}> Home Page</Link>
-      </li>
-      <li>
-        <Link to={"/gallery"}> Gallery </Link>
-      </li>
-      <li>
-        <Link to={"/aboutus"}> About Us</Link>
-      </li>
-      <li>
-        <Link to={"/links"}> Links</Link>
-      </li>
-      <li>
-        <Link to={"/contacts"}> Contakts </Link>
-      </li>
-    </ul>
+    <nav className="navigator_container">
+      <ul className="navigator_container">
+        <li>
+          <Link to={"/"}> Home Page</Link>
+        </li>
+        <li>
+          <Link to={"/gallery"}> Gallery </Link>
+        </li>
+        <li>
+          <Link to={"/aboutus"}> About Us</Link>
+        </li>
+        <li>
+          <Link to={"/links"}> Links</Link>
+        </li>
+        <li>
+          <Link to={"/contacts"}> Contakts </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
